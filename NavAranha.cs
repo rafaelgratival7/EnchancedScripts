5e304d133bc16dba5256604807ecbc73e746d3c0
rafaelgratival7 <rafael.gratival@hotmail.com>
2016-10-28 12:46:30 -0200

    Update NavAranha.cs

--- NavAranha.cs
+++ NavAranha.cs
@@ -1,28 +1,43 @@
-﻿using UnityEngine;
+using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 
-public class NavAranha : MonoBehaviour {
 
+public class NavAranha : NetworkBehaviour
+{
 	#region Declaração
 	private Animator AnimatorDaAranha;
 
-	private AudioSource[] SonsDaAranha;
+	private AudioSource SonsDaAranha;
 
 	private Collider[] PercebidosNaPercepção;
 
+	[Tooltip("Layer's dos Inimigos")]
 	public LayerMask Inimigos;
+
+	[Tooltip("Layer's dos Obstaculos")]
 	public LayerMask Obstaculos;
 
 	[HideInInspector]
 	public GameObject Projectil;
 
 	private GameObject Teia;
-	private GameObject PontoDeTiro;
+	private GameObject Ninho;
+
+	[HideInInspector]
+	public Transform PontoDeTiro;
 
 	private NavMeshAgent AgenteDaAranha;
 
+	private NavAranhaSound Clip;
+
+	[HideInInspector]
+	public NavAranhaDamager Damage;
+
 	private Quaternion Rotação;
+	private Quaternion RotaçãoParaAtack;
 
 	[HideInInspector]
 	public List<Transform> Percebidos = new List<Transform>();
@@ -31,10 +46,6 @@ public class NavAranha : MonoBehaviour {
 
 	[HideInInspector]
 	public Transform InimigoMaisProximo;
-	[HideInInspector]
-	public Transform Ninho;
-
-	private SoundManager Clip;
 
 	[HideInInspector]
 	public Vector3 NormalNinho;
@@ -44,10 +55,10 @@ public class NavAranha : MonoBehaviour {
 	private Vector3 Normal;
 	private Vector3 VelocidadeLocal;
 	private Vector3 VelocidadeGlobal;
+	private Vector3 Posição;
 
 	private bool AtacandoLonge;
 	private bool AtacandoPerto;
-	private bool InimigoPercebido;
 
 	[HideInInspector]
 	public float DireçãoDaNormal;
@@ -63,52 +74,77 @@ public class NavAranha : MonoBehaviour {
 	private float DistanciaDoMaisProximo;
 	private float DistanciaDoInimigoParaNinho;
 
+	[Tooltip("Angulo de visão da Area da Aranha")]
 	[Range(0,360)]public float AnguloDeVisão;
+
+	[Tooltip("Angulo em que o Projectil sera lançado")]
 	[Range(0,360)]public float AnguloDeTiro = 15;
-	[Range(2,1000)]public float DistanciaEntreAlvoEAranha = 5;
+
+	[Tooltip("Distancia entre o Inimigo e a Aranha")]
+	[Range(2,100)]public float DistanciaEntreAlvoEAranha = 5;
+
+	[Tooltip("Ponto inicial da Area da Aranha")]
 	[Range(0.01F,2F)]public float PontoInicial = 1F;
-	[Range(0,1000)]public float PontoMaximo = 40;
-	[Range(0,1000)]public float RaioDevisão;
+
+	[Tooltip("Ponto máximo que a Aranha pode se afastar")]
+	[Range(0,100)]public float PontoMaximo = 40;
+
+	[Tooltip("Raio da Area de Percepção da Aranha")]
+	[Range(0,100)]public float RaioDevisão;
 
 	private int index;
-	private int Temporizador;
+	private int RandomInteger;
 
-	[Range(0,10)]public int Velocidade; 
+	[Tooltip("Velocidade em que a Aranha vai rotacionar ao andar")]
+	[Range(0,15)]public int VelocidadeDeRotação;
+
+	[Tooltip("Velocidade em que a Aranha se move")]
+	[Range(0,10)]public int Velocidade;
+
+	[Range(0.1F,2F)]public int DetectionTime;
 	#endregion
 
 	#region Parte-1 Comportamento da Aranha
-	void Awake () 
+	public void Start () 
 	{
 		AgenteDaAranha = GetComponent<NavMeshAgent> ();
 		AnimatorDaAranha = GetComponent<Animator> ();
-		Ninho = transform.parent.GetComponentInParent<Transform> ();
-		SonsDaAranha = GetComponents<AudioSource> ();
+		SonsDaAranha = GetComponent<AudioSource> ();
+		Clip = GetComponent <NavAranhaSound>();
+		Teia = Resources.Load ("Prefabs/Bullet") as GameObject;
+
+		if (!isServer)
+			return;
+
+		PosiçãoDoNinho ();
 
 		AtacandoLonge = true;
 		AtacandoPerto = true;
-		InimigoPercebido = false;
 
-		Teia = Resources.Load ("Objetos/Teia") as GameObject;
-
-		PontoDeTiro = GameObject.Find ("PontoDeLançamento");
-
-		Clip = GameObject.Find ("SoundSystem").GetComponent<SoundManager> ();
-
-		StartCoroutine ("Percepção", .2f);
-		StartCoroutine ("IddleON", 1F);
+		StartCoroutine (Perception (DetectionTime));
 	}
 
-	void Update()
+	public void PosiçãoDoNinho()
 	{
+		Ninho = new GameObject ("Ninho Da Aranha");
+		Vector3 _ninhoPosition = transform.position;
+		_ninhoPosition.y = 0;
+		Quaternion _ninhoRotation = transform.rotation;
+		Ninho.transform.position = _ninhoPosition;
+		Ninho.transform.rotation = _ninhoRotation;
+	}
+
+	public void FixedUpdate()
+	{
+		if (!isServer)
+			return;
+
 		Comportamento ();
+		Atributos ();
 	}
 
-	void Comportamento ()
+	public void Comportamento ()
 	{
-		DireçãoDaFace ();
-
-		ControllerDoSom ();
-
 		if(Percebidos.Count == 0)
 		{
 			Parado ();
@@ -118,73 +154,34 @@ public class NavAranha : MonoBehaviour {
 		{
 			Perseguindo ();
 		}
-
-		DistanciaEmRadius = (transform.position - Ninho.position).sqrMagnitude;
 	}
 
-	IEnumerator AtackLongoAtivo()
+	public void Atributos()
 	{
-		SonsDaAranha [0].PlayOneShot (Clip.NPC_Aranha [4]);
-
-		yield return new WaitForSeconds (AnimatorDaAranha.GetCurrentAnimatorStateInfo(0).length);
-
-		SonsDaAranha [0].Stop ();
-
-		AtacandoLonge = true;
-
-		if(InimigoMaisProximo != null && DistanciaEmRadius > PontoMaximo)
-		{
-			Projectil = Instantiate (Teia, PontoDeTiro.transform.position, Quaternion.LookRotation(InimigoMaisProximo.position)) as GameObject;
-
-			Projectil.transform.LookAt (InimigoMaisProximo.position);
-
-			Seno = Mathf.Sqrt(DistanciaDoMaisProximo* -Physics.gravity.y / (Mathf.Sin(Mathf.Deg2Rad * AnguloDeTiro * 2)));
-
-			Tangente = Seno * Mathf.Sin(Mathf.Deg2Rad * AnguloDeTiro);
-			Cosseno = Seno * Mathf.Cos(Mathf.Deg2Rad * AnguloDeTiro);
-
-			VelocidadeLocal = new Vector3(0f, Tangente, Cosseno);
-
-			VelocidadeGlobal = transform.TransformVector(VelocidadeLocal);
-
-			Projectil.GetComponent<Rigidbody>().velocity = VelocidadeGlobal;
-		}
-	}
-
-	IEnumerator AtackCurtoAtivo()
-	{
-		SonsDaAranha [0].PlayOneShot (Clip.NPC_Aranha [3]);
-
-		yield return new WaitForSeconds (AnimatorDaAranha.GetCurrentAnimatorStateInfo(0).length);
-
-		SonsDaAranha [0].Stop ();
-
-		AtacandoPerto = true;
-
-		//Dano do atack curto aqui
-	}
-
-	void DireçãoDaFace()
-	{
+		//Normal da Aranha
 		Normal = transform.forward;
 		Normal.y = 0;
 		DireçãoDaNormal = Mathf.Round(Quaternion.LookRotation(Normal).eulerAngles.y);
 
+		//Normal do Ninho
 		NormalNinho = Ninho.transform.forward;
 		NormalNinho.y = 0;
 		DireçãoDoNinho = Mathf.Round (Quaternion.LookRotation (NormalNinho).eulerAngles.y);
+
+		//Distancia Da Aranha para Ninho
+		DistanciaEmRadius = (transform.position - Ninho.transform.position).sqrMagnitude;
 	}
 
-	void Parado ()
+	public void Parado ()
 	{
 
-		AgenteDaAranha.SetDestination (Ninho.position);
+		AgenteDaAranha.SetDestination (Ninho.transform.position);
 
 		AgenteDaAranha.speed = Velocidade;
 		AgenteDaAranha.stoppingDistance = 0;
 
 		AnimatorDaAranha.SetBool ("Atack_Longo", false);
-		AnimatorDaAranha.SetBool ("Atack_Curto", false);
+		AnimatorDaAranha.SetInteger("Atack_Curto", 0);
 
 		InimigoMaisProximo = null;
 
@@ -194,15 +191,14 @@ public class NavAranha : MonoBehaviour {
 
 			if(DireçãoDaNormal != DireçãoDoNinho)
 			{
-				transform.rotation = Quaternion.Slerp (transform.rotation, Ninho.rotation, Time.deltaTime * 3);
+				transform.rotation = Quaternion.Slerp (transform.rotation, Ninho.transform.rotation, Time.deltaTime * 3);
 			}
 
 			if(DireçãoDaNormal == DireçãoDoNinho)
 			{
-				SonsDaAranha [1].Stop ();
 				AnimatorDaAranha.SetBool ("Andando",false);
 			}
-				
+
 		}
 		else
 		{
@@ -210,17 +206,16 @@ public class NavAranha : MonoBehaviour {
 		}
 	}
 
-	void Perseguindo ()
+	public void Perseguindo ()
 	{
 
 		DistanciaDoMaisProximo = Vector3.Distance (transform.position, InimigoMaisProximo.position);
 
-		DistanciaDoInimigoParaNinho = (InimigoMaisProximo.position - Ninho.position).sqrMagnitude;
+		DistanciaDoInimigoParaNinho = (InimigoMaisProximo.position - Ninho.transform.position).sqrMagnitude;
 
 		AgenteDaAranha.SetDestination (InimigoMaisProximo.position);
-	
-		AnimatorDaAranha.SetBool ("Andando",true);
 
+		AnimatorDaAranha.SetBool ("Andando",true);
 
 		if(DistanciaEmRadius > PontoMaximo)
 		{
@@ -231,14 +226,11 @@ public class NavAranha : MonoBehaviour {
 			if(DistanciaDoInimigoParaNinho > PontoMaximo)
 			{
 
-				SonsDaAranha [1].Stop ();
 				AgenteDaAranha.speed = 0;
 				AgenteDaAranha.stoppingDistance = RaioDevisão;
 				AgenteDaAranha.velocity = Vector3.zero;
-
 				AnimatorDaAranha.SetBool ("Atack_Longo", true);
-
-				transform.LookAt (InimigoMaisProximo);
+				transform.LookAt (InimigoMaisProximo.position);
 
 				if(AtacandoLonge)
 				{
@@ -268,15 +260,13 @@ public class NavAranha : MonoBehaviour {
 
 			if(DistanciaDoMaisProximo < DistanciaEntreAlvoEAranha)
 			{
-				AnimatorDaAranha.SetBool ("Atack_Curto", true);
+				RandomInteger = Random.Range (1, 3);
+				AnimatorDaAranha.SetInteger("Atack_Curto", RandomInteger);
 
-				SonsDaAranha [1].Stop ();
 				AgenteDaAranha.stoppingDistance = DistanciaEntreAlvoEAranha - 1;
 				AgenteDaAranha.speed = 0;
 				AgenteDaAranha.velocity = Vector3.zero;
-
-
-				transform.LookAt (InimigoMaisProximo);
+				transform.LookAt (InimigoMaisProximo.position);
 
 				if(AtacandoPerto)
 				{
@@ -288,22 +278,66 @@ public class NavAranha : MonoBehaviour {
 			if(DistanciaDoMaisProximo >= DistanciaEntreAlvoEAranha)
 			{
 
-				AnimatorDaAranha.SetBool ("Atack_Curto", false);
-
+				AnimatorDaAranha.SetInteger("Atack_Curto", 0);
 				AgenteDaAranha.stoppingDistance = 0;
 				AgenteDaAranha.speed = Velocidade;
 			}
 		}
-			
+
+	}
+
+	public IEnumerator AtackLongoAtivo()
+	{
+		yield return new WaitForSeconds (AnimatorDaAranha.GetCurrentAnimatorStateInfo(0).length - 0.2F);
+
+		AtacandoLonge = true;
+
+		if(InimigoMaisProximo != null && DistanciaEmRadius > PontoMaximo)
+		{
+			Vector3 Direction = (transform.position - InimigoMaisProximo.position).normalized;
+			Projectil = Instantiate (Teia, PontoDeTiro.position, Quaternion.LookRotation(-Direction)) as GameObject;
+			NetworkServer.Spawn (Projectil);
+
+			Seno = Mathf.Sqrt(DistanciaDoMaisProximo* -Physics.gravity.y / (Mathf.Sin(Mathf.Deg2Rad * AnguloDeTiro * 2)));
+
+			Tangente = Seno * Mathf.Sin(Mathf.Deg2Rad * AnguloDeTiro);
+			Cosseno = Seno * Mathf.Cos(Mathf.Deg2Rad * AnguloDeTiro);
+
+			VelocidadeLocal = new Vector3(0f, Tangente, Cosseno);
+
+			VelocidadeGlobal = transform.TransformVector(VelocidadeLocal);
+
+			Projectil.GetComponent<Rigidbody>().AddForce(-Direction * 50,ForceMode.Force);
+
+			Projectil.GetComponent<Rigidbody>().velocity = VelocidadeGlobal;
+
+			Projectil.transform.position += Projectil.transform.forward * 1 * Time.deltaTime;
+		}
+	}
+
+	public IEnumerator AtackCurtoAtivo()
+	{
+
+		yield return new WaitForSeconds (AnimatorDaAranha.GetCurrentAnimatorStateInfo(0).length - 0.2F);
+
+		if(Damage.isHere)
+		{
+			Damage.isThis.GetComponent <Health>().TakeDamage (10,this.gameObject);
+		}
+
+		AtacandoPerto = true;
+
+		//Chamada de atack curto + Efeito
+		//Utilities.PlayEffectAt ()
 	}
 	#endregion
 
 	#region Parte-2 Detectar o mais próximo inimigo
-	IEnumerator Percepção(float Atraso)		
+	public IEnumerator Perception(float Time)		
 	{
 		while(true)
 		{
-			yield return new WaitForSeconds (1.2f);
+			yield return new WaitForSeconds (Time);
 			Percebido();
 		}
 	}
@@ -333,7 +367,7 @@ public class NavAranha : MonoBehaviour {
 		}
 	}
 
-	Transform PegarInimigoMaisProximo (List<Transform> InimigosPercebidos)
+	public Transform PegarInimigoMaisProximo (List<Transform> InimigosPercebidos)
 	{
 		InimigoMaisProximo = null;
 
@@ -363,50 +397,6 @@ public class NavAranha : MonoBehaviour {
 			AngulosEmGraus += transform.eulerAngles.y;
 		}
 		return new Vector3 (Mathf.Sin (AngulosEmGraus * Mathf.Deg2Rad), 0, Mathf.Cos (AngulosEmGraus * Mathf.Deg2Rad));
-	}
-	#endregion
-
-	#region Parte-3 Controlador de Som da Aranha
-	void ControllerDoSom()
-	{
-		if (AgenteDaAranha.velocity.magnitude > 2F && !SonsDaAranha[1].isPlaying)
-		{
-			SonsDaAranha[1].PlayOneShot (Clip.NPC_Aranha [1]);
-			SonsDaAranha[1].loop = true;
-		}
-		else
-		{
-			SonsDaAranha[1].loop = false;
-		}
-
-		if(InimigoMaisProximo != null)
-		{
-			if(InimigoPercebido)
-			{
-				InimigoPercebido = false;
-				SonsDaAranha[1].PlayOneShot (Clip.NPC_Aranha [2]);
-			}
-		}
-		else
-		{
-			InimigoPercebido = true;
-		}
-	}
-		
-	IEnumerator IddleON()
-	{
-		while(true)
-		{
-			Temporizador = Random.Range (8, 15);
-
-			yield return new WaitForSeconds (Temporizador);
-
-			if(AnimatorDaAranha.GetCurrentAnimatorStateInfo(0).IsName("Aranha_Iddle") && !AnimatorDaAranha.IsInTransition(0))
-			{
-				SonsDaAranha[0].PlayOneShot (Clip.NPC_Aranha [0]);
-				SonsDaAranha [0].volume = 0.2F;
-			}
-		}
 	}
 	#endregion
 }
