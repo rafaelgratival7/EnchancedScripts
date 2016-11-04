d8f03b3f8587063c25e8f1832bddf1f01a466398
rafaelgratival7 <rafael.gratival@hotmail.com>
2016-11-04 20:14:47 -0200

    Add files via upload

---
+++ CameraLmanager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+ public class CameraLmanager : MonoBehaviour {
+
+    #region Declaração
+	public Camera[] ListaDeCamaras;
+	private int Contador;
+    #endregion
+
+	void Start()
+	{
+		for(int i = 0; i < ListaDeCamaras.Length;i++)
+		{
+			if(i == 0)
+			{
+				ListaDeCamaras [i].enabled = true;
+			}
+			else
+			{
+				ListaDeCamaras [i].enabled = false;
+			}
+		}
+	}
+
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+
+			if (Contador > 0) 
+			{
+				Contador--;
+				ListaDeCamaras [Contador].enabled = true;
+				ListaDeCamaras [Contador + 1].enabled = false;
+			} 
+		}
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+
+			if (Contador < ListaDeCamaras.Length - 1) 
+			{
+				Contador++;
+				ListaDeCamaras [Contador].enabled = true;
+			} 
+		}
+    }
+}
+
+[CustomEditor(typeof(CameraLmanager))]
+public class CameraEditor : Editor{
+
+	public override void OnInspectorGUI()
+	{
+		EditorGUILayout.HelpBox ("Apertar Q para ultima Camera , Apertar E para proxima Camera, Lembrar de ativar todas as Cameras", MessageType.Warning);
+	}
+}
