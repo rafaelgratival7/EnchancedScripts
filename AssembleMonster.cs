2357367ff89053083b079e0ca6ca2dccb2228bf2
rafaelgratival7 <rafael.gratival@hotmail.com>
2016-11-04 20:14:21 -0200

    Add files via upload

---
+++ AssembleMonster.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEditorInternal;
+using UnityEditor.VersionControl;
+
+public class AssembleMonster : MonoBehaviour 
+{
+	public GameObject Monster;
+	public Animator MonsterAnimator;
+	public AnimationClip MonsterClip;
+	public List<string> ClipNames = new List<string> ();
+	public AnimationClip[] Try;
+	public ModelImporter MonstersClip;
+	public List<AnimationClip> clipList = new List <AnimationClip>();
+
+
+	// Use this for initialization
+	void Awake () 
+	{
+		Monster = GameObject.FindGameObjectWithTag ("Monster");
+		MonstersClip = (ModelImporter)AssetImporter.GetAtPath ("Assets/Resources/NPC's/Original/Aranhas/aranha_Boss");
+	}
+
+	void Start()
+	{
+		MonsterAnimator = Monster.GetComponent <Animator> ();
+		MonsterAnimator.runtimeAnimatorController = Resources.Load ("Controllers/All") as RuntimeAnimatorController;
+
+		foreach(AnimationClip clip in MonsterAnimator.runtimeAnimatorController.animationClips)
+		{
+			ClipNames.Add (clip.name);
+		}
+	}  
+
+	void OnPostprocessModel(GameObject go)
+	{        
+		AnimationClip[] clips = AnimationUtility.GetAnimationClips(go);
+
+		foreach (AnimationClip clip in clips)
+		{
+			AnimationClip newClip = new AnimationClip();
+			clipList.Add(newClip);
+		}
+	}  
+}
